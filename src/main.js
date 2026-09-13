0f8ed3f49c40609910d73374cbea72440d187583
Stanislav <vasukovski@yahoo.com>
2026-09-13 17:04:27 +0100

    fix form class

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import {
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.search-form');
+const form = document.querySelector('.form');
 
 const loadMoreButton = document.querySelector('.load-more');
 
